7719d87ff4f885b5ca941add0b2d49a3ed2a4792
Mike <mikej.miller440@gmail.com>
2022-03-02 14:43:40 -0600

    added user schema

--- models/User.js
+++ models/User.js
@@ -6,8 +6,58 @@ const sequelize = require("../config/connection");
 class User extends Model {}
 
 User.init(
-	{},
 	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [4]
+			}
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [4]
+			}
+		},
+		location_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "Location",
+				key: id
+			}
+		},
+		comment_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "comment",
+				key: "id"
+			}
+		},
+		likes_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "likes",
+				key: "id"
+			}
+		}
+	},
+	{
+		// TODO: Add hook to bcrypt password before create
 		sequelize,
 		timestamps: false,
 		freezeTableName: true,
