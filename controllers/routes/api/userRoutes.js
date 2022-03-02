0431bb4f075189a50ddafa56388d8509fbc69a45
coyote <megan.metelak@gmail.com>
2022-03-02 13:30:54 -0600

    started user and location routes

---
+++ controllers/routes/api/userRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const { User, Location, Comment, Rating } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+module.exports = router;
