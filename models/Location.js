8f116d742900f81a1c6aedd7c20c5a2a58f94084
Mike <mikej.miller440@gmail.com>
2022-03-02 14:34:09 -0600

    added more packages to package.json and edited based on eslint rules

--- models/Location.js
+++ models/Location.js
@@ -1,21 +1,19 @@
 // import important parts of sequelize lib
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 //import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class Location extends Model {}
 
 Location.init(
-    {
-
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Location',
-    }
-)
+	{},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "Location"
+	}
+);
 
 module.exports = Location;
--- models/User.js
+++ models/User.js
@@ -1,21 +1,19 @@
 // import important parts of sequelize lib
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 //import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class User extends Model {}
 
 User.init(
-    {
-
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'User',
-    }
-)
+	{},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "User"
+	}
+);
 
 module.exports = User;
