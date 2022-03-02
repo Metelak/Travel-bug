9c50574b83c2b71254f914cc67f0f0915e7a993e
coyote <megan.metelak@gmail.com>
2022-03-02 14:22:50 -0600

    added id, comment_text, user_id, location_id to comment model

--- models/Comment.js
+++ models/Comment.js
@@ -6,8 +6,34 @@ class Comment extends Model {}
 
 Comment.init(
     {
-
-    },
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        comment_text: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'user',
+            key: 'id'
+          }
+        },
+        location_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'location',
+            key: 'id'
+          }
+        }
+      },
     {
         sequelize,
         timestamps: false,
