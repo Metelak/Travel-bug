9061093263792d77310e085d4b7c5eee6e29f53b
Mike <mikej.miller440@gmail.com>
2022-03-04 14:39:33 -0600

    added a seedAll function

--- seeds/index.js
+++ seeds/index.js
@@ -5,3 +5,29 @@ const seedRatings = require("./seedRating");
 const seedLikes = require("./seedLike");
 
 const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+	try {
+		await sequelize.sync({ force: true });
+		console.log("\n----- DATABASE SYNCED -----\n");
+
+		await seedUsers();
+		console.log("\n----- USERS SYNCED -----\n");
+
+		await seedLocations();
+		console.log("\n----- LOCATIONS SYNCED -----\n");
+
+		await seedComments();
+		console.log("\n----- COMMENTS SYNCED -----\n");
+
+		await seedRatings();
+		console.log("\n----- RATINGS SYNCED -----\n");
+
+		await seedLikes();
+		console.log("\n----- LIKES SYNCED -----\n");
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+seedAll();
