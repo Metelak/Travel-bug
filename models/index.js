78b0cb5e6bb824ff0eaf2810b5644650971dc2da
Mike <mikej.miller440@gmail.com>
2022-03-03 20:00:02 -0600

    added associations

--- models/index.js
+++ models/index.js
@@ -5,11 +5,51 @@ const Comment = require("./Comment");
 const Location = require("./Location");
 const Like = require("./Like");
 
-// ex. hasMany
+// User associations
 
-// ex. belongsToMany
+User.hasMany(Rating, {
+	foreignKey: "user_id"
+});
 
-// ect.
+Rating.belongsTo(User, {
+	foreignKey: "user_id"
+});
+
+User.hasMany(Location, {
+	foreignKey: "user_id"
+});
+
+Location.belongsTo(User, {
+	foreignKey: "user_id"
+});
+
+User.hasMany(Comment, {
+	foreignKey: "comment_id"
+});
+
+Comment.belongsTo(User, {
+	foreignKey: "comment_id"
+});
+
+// Like associations
+
+User.belongsToMany(Location, {
+	through: Like
+});
+
+Location.belongsToMany(User, {
+	through: Like
+});
+
+// Location Associations
+
+Location.hasMany(Comment, {
+	foreignKey: "location_id"
+});
+
+Comment.belongsTo(Location, {
+	foreignKey: "location_id"
+});
 
 module.exports = {
 	User,
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 //const session = require('express.session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers/routes');
-const sequelize = require('./config/connection');
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/routes");
+const sequelize = require("./config/connection");
 //const path = require('path');
 //const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -31,10 +30,9 @@ app.use(routes);
 
 const hb = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log("Now listening"));
 });
