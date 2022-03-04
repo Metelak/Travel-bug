092b8273a66048d00e1523e63046a4052cdd790e
Mike <mikej.miller440@gmail.com>
2022-03-04 14:34:32 -0600

    imported variables into index.js

---
+++ seeds/index.js
@@ -0,0 +1,7 @@
+const seedUsers = require("./seedUser");
+const seedLocations = require("./seedLocation");
+const seedComments = require("./seedComment");
+const seedRatings = require("./seedRating");
+const seedLikes = require("./seedLike");
+
+const sequelize = require("../config/connection");
