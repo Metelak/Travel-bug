cdff590c6233cfa0ace787efdcfa75f00538047d
Mike <mikej.miller440@gmail.com>
2022-03-05 14:29:50 -0600

    added get all route

--- controllers/routes/api/ratingRoutes.js
+++ controllers/routes/api/ratingRoutes.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
-const { Comment, User, Location } = require("../../../models");
+const { Comment, User, Location, Rating } = require("../../../models");
+
+router.get("/", (req, res) => {
+	Rating.findAll({
+		include: [
+			{
+				model: User,
+				attributes: ["id", "username", "email"]
+			},
+			{
+				model: Location,
+				attributes: ["id", "title", "text", "user_id"],
+				include: {
+					model: User,
+					attributes: ["username", "email"]
+				}
+			}
+		]
+	});
+});
