641b7c67ec502b0e53e373df131aa77965d4e7e1
Mike <mikej.miller440@gmail.com>
2022-03-05 13:52:14 -0600

    created get requests for locations

--- controllers/routes/api/locationRoutes.js
+++ controllers/routes/api/locationRoutes.js
@@ -1,5 +1,59 @@
 const router = require("express").Router();
-// const sequelize = require('../../config/connection');
 const { Location, User, Comment, Rating } = require("../../../models");
 
+router.get("/", (req, res) => {
+	Location.findAll({
+		include: [
+			{
+				model: User,
+				attributes: ["id", "username", "email"]
+			},
+			{
+				model: Rating,
+				attributes: ["id", "rating", "user_id"]
+			},
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "user_id"]
+			}
+		]
+	})
+		.then((dbLoctaionData) => {
+			res.json(dbLoctaionData);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+router.get("/:id", (req, res) => {
+	Location.findOne({
+		where: {
+			id: req.params.id
+		},
+		include: [
+			{
+				model: User,
+				attributes: ["id", "username", "email"]
+			},
+			{
+				model: Rating,
+				attributes: ["id", "rating", "user_id"]
+			},
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "user_id"]
+			}
+		]
+	})
+		.then((dbLoctaionData) => {
+			if (!dbLoctaionData) {
+				res.status(404).json({ message: "No location found with this id" });
+			}
+			res.json(dbLoctaionData);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
 module.exports = router;
