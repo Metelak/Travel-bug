14c870781857d35d69b3edf3565734b725e6555c
Mike <mikej.miller440@gmail.com>
2022-03-05 14:45:02 -0600

    added /ratings to the index.js

--- controllers/routes/api/index.js
+++ controllers/routes/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const commentRoutes = require("./commentRoutes");
 const locationRoutes = require("./locationRoutes");
 const userRoutes = require("./userRoutes");
+const ratingRoutes = require("./ratingRoutes");
 
 router.use("/users", userRoutes);
 router.use("/locations", locationRoutes);
 router.use("/comments", commentRoutes);
+router.use("/ratings", ratingRoutes);
 
 module.exports = router;
