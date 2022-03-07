bdf3ea929cbeea4d7659c4ad61d66bed91ec5b6f
Mike <mikej.miller440@gmail.com>
2022-03-06 20:34:09 -0600

    completed function

--- utils/location-pictures.js
+++ utils/location-pictures.js
@@ -1,17 +1,19 @@
-const { Location } = require("../models");
 const auth = process.env.UNSPLASH_KEY;
 
+// returns the first image result of the location
 const getPicture = async (location) => {
 	try {
-		// retreive first result based on location name
+		// retreive first result based on location name (50 requests per hour)
 		const response = await fetch(
-			`https://api.unsplash.com/photos/random?client_id=${auth}&query=${location}&orientation=landscape`
+			`https://api.unsplash.com/search/photos?client_id=${auth}&query=${location}&orientation=landscape`
 		);
 		if (response.ok) {
 			let unsplashObj = response.json();
+			return unsplashObj.results[0].urls.raw;
 		}
 	} catch (err) {
 		console.log(err);
+		return false;
 	}
 };
 
