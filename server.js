d383c99da84ce077321569b29c2be590d02f931c
Mr WhiteKidd <austinslater58@gmail.com>
2022-03-07 17:38:52 -0600

    session implements added helpers in utils and imported it to server.js added underscore to format_plural in location-ingo.handlebars

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require("express");
-//const session = require('express.session');
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/routes");
 const sequelize = require("./config/connection");
 const path = require("path");
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
+
+app.use(session(sess));
+
+//handlebars
+
+const hbs = exphbs.create({helpers});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,12 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Use apiRoutes
 app.use(routes);
 
-//handlebars
 
-const hbs = exphbs.create({});
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
