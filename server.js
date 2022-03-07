be36bd5cdf09004841d6772c2e34daa84feb4152
Mike <mikej.miller440@gmail.com>
2022-03-07 14:38:27 -0600

    created an api route with the location-picture content

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/routes");
 const sequelize = require("./config/connection");
-//const path = require('path');
+const path = require("path");
 //const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use apiRoutes
 app.use(routes);
