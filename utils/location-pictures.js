ba16f6aa17751579c1caa9c262beb3218fef4caf
Mike <mikej.miller440@gmail.com>
2022-03-06 21:02:03 -0600

    added an option to edit the photos

--- utils/location-pictures.js
+++ utils/location-pictures.js
@@ -4,16 +4,22 @@ const auth = process.env.UNSPLASH_KEY;
 const getPicture = async (location) => {
 	try {
 		// retreive first result based on location name (50 requests per hour)
+		// optional orientations are landscape, portrait, squarish
 		const response = await fetch(
 			`https://api.unsplash.com/search/photos?client_id=${auth}&query=${location}&orientation=landscape`
 		);
 		if (response.ok) {
-			let unsplashObj = response.json();
-			return unsplashObj.results[0].urls.raw;
+			let photoUrl = response.json().results[0].urls.raw;
+			// edit w (width) and h (height) to be whatever you'd like
+			let editedphoto = `${photoUrl}&w=500&h=300`;
+			return editedphoto;
+		} else {
+			// We could return another photo if there are no results...
+			return;
 		}
 	} catch (err) {
 		console.log(err);
-		return false;
+		return;
 	}
 };
 
