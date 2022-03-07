038553872b23f244165fbc8cf8fad8a4a67560f6
Mr WhiteKidd <austinslater58@gmail.com>
2022-03-06 18:30:14 -0600

    all comment routes operational all user routes work, could not get by id

--- controllers/routes/api/commentRoutes.js
+++ controllers/routes/api/commentRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Comment, User, Location } = require("../../../models");
 // const sequelize = require('../../config/connection');
-const withAuth = require("../../../utils/auth");
+//const withAuth = require("../../../utils/auth");
 
 //GET all comments
 router.get("/", (req, res) => {
@@ -32,7 +32,36 @@ router.get("/", (req, res) => {
 		});
 });
 
-router.put("/:id", withAuth, (req, res) => {
+router.get("/:id", (req, res) => {
+	Comment.findOne({
+		where: {
+			id: req.params.id
+		},
+		include: [
+			{
+				model: User,
+				attributes: ["id", "username", "email"]
+			},
+			{
+				model: Location,
+				attributes: ["id", "title", "text", "user_id"]
+			}
+		]
+	})
+		.then((dbUserData) => {
+			if (!dbUserData) {
+				res.status(404).json({ message: "No user found with this id" });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+// withAuth needs to be implemented
+router.put("/:id", (req, res) => {
 	//update a comment by its 'id' value
 	Comment.update(req.body, {
 		where: {
@@ -52,40 +81,41 @@ router.put("/:id", withAuth, (req, res) => {
 		});
 });
 
-router.post("/", withAuth, (req, res) => {
+//add withAuth
+router.post("/", (req, res) => {
 	// expects => {comment_text: "This is the comment", user_id: 1, location_id: 2}
-	if (req.session) {
-		Comment.create({
-			comment_text: req.body.comment_text,
-			user_id: req.session.user_id,
-			location_id: req.body.location_id
-		})
-			.then((dbCommentData) => res.json(dbCommentData))
-			.catch((err) => {
-				console.log(err);
-				res.status(400).json(err);
-			});
-	}
+	//if (req.session) {
+	Comment.create({
+		comment_text: req.body.comment_text,
+		user_id: req.body.user_id,
+		location_id: req.body.location_id
+	})
+		.then((dbCommentData) => res.json(dbCommentData))
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json(err);
+		});
+	//}
 });
 
 router.delete("/:id", (req, res) => {
-	if (req.session) {
-		Comment.destroy({
-			where: {
-				id: req.params.id
+	//if (req.session) {
+	Comment.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+		.then((dbCommentData) => {
+			if (!dbCommentData) {
+				res.status(404).json({ message: "No comment found with this id!" });
+				return;
 			}
+			res.json(dbCommentData);
 		})
-			.then((dbCommentData) => {
-				if (!dbCommentData) {
-					res.status(404).json({ message: "No comment found with this id!" });
-					return;
-				}
-				res.json(dbCommentData);
-			})
-			.catch((err) => {
-				console.log(err);
-				res.status(500).json(err);
-			});
-	}
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+	//}
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,32 @@
+const path = require("path");
 const express = require("express");
-//const session = require('express.session');
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+
 const routes = require("./controllers/routes");
 const sequelize = require("./config/connection");
-//const path = require('path');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(require("./controllers/routes"));
 // Use apiRoutes
 app.use(routes);
 
