28034e0f2ab4567e13135a9e366e049667224f3e
Mike <mikej.miller440@gmail.com>
2022-03-06 20:18:22 -0600

    passed in queries to unsplash api

--- utils/location-pictures.js
+++ utils/location-pictures.js
@@ -1,9 +1,18 @@
 const { Location } = require("../models");
-
 const auth = process.env.UNSPLASH_KEY;
 
-// Unsplash url
-// !Only allows 50 requests per hour
-const unsplash = "https://api.unsplash.com/photos/random";
+const getPicture = async (location) => {
+	try {
+		// retreive first result based on location name
+		const response = await fetch(
+			`https://api.unsplash.com/photos/random?client_id=${auth}&query=${location}&orientation=landscape`
+		);
+		if (response.ok) {
+			let unsplashObj = response.json();
+		}
+	} catch (err) {
+		console.log(err);
+	}
+};
 
-const getPicture = (location) => {};
+module.exports = getPicture;
