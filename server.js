d67c3c52586c43b30aa44cc91ce8443cac28beed
coyote <megan.metelak@gmail.com>
2022-03-07 16:33:04 -0600

    new PR

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // turn on path to public folder - stylesheets
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Use apiRoutes
 app.use(routes);
