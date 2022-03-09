c038388ebcea84333839221810e67b790c08e6d5
Mr WhiteKidd <austinslater58@gmail.com>
2022-03-08 20:44:27 -0600

    merged recent develop push

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Use apiRoutes
@@ -42,3 +42,5 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
+
+console.log(NaN);
